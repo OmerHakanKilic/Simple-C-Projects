69bc3efd991487d04e0866147cb889dd35465134
OmerHakanKilic <156901186+OmerHakanKilic@users.noreply.github.com>
2025-08-30 01:44:00 +0300

    Add files via upload

--- simple-cryptography.c
+++ simple-cryptography.c
@@ -1,86 +1,72 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 //65 - 90 A - Z, 97 - 122 a - zs
 
 int normalize_key(int key)
 {
-    key = key % 26;
-    return key;
+    return key % 26;
 }
 char fit_in_alphabet_lower_case(char c)
 {
-    if(c >= 97 && c <= 122)
+    while (c < 97)
     {
-        return c;
+        c += 26;
     }
-    else if(c >= 97)
+    while (c > 122)
     {
-        return fit_in_alphabet_lower_case(-26);
+        c -= 26;
     }
-    else if(c <= 122)
-    {
-        return fit_in_alphabet_lower_case(+26);
-    }
-    else
-    {
-        return '!';
-    }
+    return c;
 }
 
 char fit_in_alphabet_upper_case(char c)
 {
-    if (c >= 65 && c <= 90)
+    while (c < 65)
     {
-        return c;
+        c += 26;
     }
-    else if(c >= 65 )
+    while (c > 90)
     {
-        return fit_in_alphabet_upper_case(c-26);
+        c -= 26;
     }
-    else if(c <= 90)
-    {
-        return fit_in_alphabet_upper_case(c+26);
-    }
-    else
-    {
-        return '!';
-    }
+    return c;
 }
 
 char* decyper()
 {
-    printf("To cypher you need to enter a text and key\nPlease enter your text:\n");
     char* plaintext;
     size_t buffer_size=0;
     ssize_t input_size;
     int key = 0;
+
+    printf("To cypher you need to enter a text and key\nPlease enter your text:\n");
     input_size = getline(&plaintext, &buffer_size, stdin);
     plaintext[strcspn(plaintext, "\n")] = 0;
+
     printf("input size:%zu\n", input_size);
+
     printf("Please enter your key: \n");
     scanf("%d", &key);
+    while (getchar() != '\n');
     key = normalize_key(key);
+
     for (int i = 0;i <input_size-1 ; i++)
     {
-        if(plaintext[i] == 32)
+        if(plaintext[i] == ' ')
         {
             continue;
         }
-        plaintext[i] -= key;
-        if (plaintext[i] >= 65-key && plaintext[i] <= 90-key)
+        else if (plaintext[i] >= 65 && plaintext[i] <= 90)
         {
-            if(plaintext[i] > 90)
-            {
-                plaintext[i] = fit_in_alphabet_upper_case(plaintext[i]);
-            }
+            plaintext[i] -= key;
+            plaintext[i] = fit_in_alphabet_upper_case(plaintext[i]);
         }
-        else if(plaintext[i] >= 97-key && plaintext[i] <= 122-key)
+        else if(plaintext[i] >= 97 && plaintext[i] <= 122)
         {
-            if(plaintext[i] > 122)
-            {
-                plaintext[i] =fit_in_alphabet_lower_case(plaintext[i]);
-            }
+            plaintext[i] -= key;
+            plaintext[i] =fit_in_alphabet_lower_case(plaintext[i]);
         }
         else
         {
@@ -94,38 +80,40 @@ char* cypher()
 {
     printf("To cypher you need to enter a text and key\nPlease enter your text:\n");
     char* plaintext;
-    size_t buffer_size=0;
+    size_t buffer_size = 0;
     ssize_t input_size;
     int key = 0;
+
     input_size = getline(&plaintext, &buffer_size, stdin);
     plaintext[strcspn(plaintext, "\n")] = 0;
+
     printf("input size:%zu\n", input_size);
     printf("Please enter your key: \n");
+
     scanf("%d", &key);
+    while (getchar() != '\n');
     key = normalize_key(key);
+    printf("Normalized key: %d\n", key);
+
     for (int i = 0;i <input_size-1 ; i++)
     {
-        if(plaintext[i] == 32)
+        if(plaintext[i] == ' ')
         {
             continue;
         }
-        plaintext[i] += key;
-        if (plaintext[i] >= 65+key && plaintext[i] <= 90+key)
+        else if (plaintext[i] >= 65 && plaintext[i] <= 90)
         {
-            if(plaintext[i] > 90)
-            {
-                plaintext[i] = fit_in_alphabet_upper_case(plaintext[i]);
-            }
+            plaintext[i] += key;
+            plaintext[i] = fit_in_alphabet_upper_case(plaintext[i]);
         }
-        else if(plaintext[i] >= 97+key && plaintext[i] <= 122+key)
+        else if(plaintext[i] >= 97 && plaintext[i] <= 122)
         {
-            if(plaintext[i] > 122)
-            {
-                plaintext[i] =fit_in_alphabet_lower_case(plaintext[i]);
-            }
+            plaintext[i] += key;
+            plaintext[i] =fit_in_alphabet_lower_case(plaintext[i]);
         }
         else
         {
+            printf("Error: %c with id:%d is invalid\n", plaintext[i],plaintext[i]);
             return "Error: invalid input\n";
         }
     }
@@ -137,6 +125,7 @@ void menu()
     printf("To cypher press 1\nTo decyper press 2\n");
     int choice =0;
     scanf("%d",&choice);
+
     while (getchar() != '\n');
     switch (choice)
     {
